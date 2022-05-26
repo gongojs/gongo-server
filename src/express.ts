c163b96f5fadf00aefc6d40ef4227d170f26c467
Gadi Cohen <dragon@wastelands.net>
2022-05-26 15:03:10 +0100

    use arson for req/res

--- src/express.ts
+++ src/express.ts
@@ -8,19 +8,31 @@ export async function GSExpressPost(
   req: Request,
   res: Response
 ): Promise<void> {
-  const $gongo = req.body.$gongo;
+  //console.log(JSON.stringify(req.body, null, 2));
+  //const $gongo = req.body.$gongo;
+  let query;
+  try {
+    query = this.ARSON.decode(req.body);
+  } catch (error) {
+    console.error("Error decoding: " + req.body);
+    console.error(error);
+    res.status(400).send("Bad Request");
+    res.end();
+    return;
+  }
+  console.log(query);
 
-  if (!$gongo) {
+  if (!query.$gongo) {
     //res.sendStatus(400); // not available in vercel
     res.status(400).send("Bad Request");
     res.end();
     return;
   }
 
-  if ($gongo !== 2) {
+  if (query.$gongo !== 2) {
     res.json({
       $error: {
-        message: `Unsupported API version.  Requested: ${$gongo}, Available: 2`,
+        message: `Unsupported API version.  Requested: ${query.$gongo}, Available: 2`,
       },
     });
     res.end();
@@ -33,24 +45,26 @@ export async function GSExpressPost(
     // @ts-expect-error: think more how to handle this TODO
     dba: this.dba,
     // @ts-expect-error: think more how to handle this TODO
-    auth: new Auth(this.dba, req.body.auth),
+    auth: new Auth(this.dba, query.auth),
   };
 
   const out = {
     calls: [] as Array<MethodResult>,
   };
 
-  if (req.body.calls)
+  if (query.calls)
     out.calls = await Promise.all(
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      req.body.calls.map(([name, query]: [string, any]) =>
+      query.calls.map(([name, query]: [string, any]) =>
         this._methodExec(name, query, props)
       )
     );
 
   // console.log(req.body);
-  // console.log(out);
+  console.log(out);
+  //res.json(out);
 
-  res.json(out);
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  res.send(this.ARSON.encode(out));
   res.end();
 }
